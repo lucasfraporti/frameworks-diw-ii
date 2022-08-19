ddf1dbc5f3adeee404866b24e7bd6189ee5539f7
lucasfraporti <lucasmfraporti@gmail.com>
2022-08-18 21:52:04 -0300

    new routes

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
+const port = 8001;
 
-app.get('/', (req, res) => {
-    return res.json({mensagem: 'OK'});
+app.get('/produtos', (req, res) => {
+    return res.status(200).send('Listando os produtos!');
 });
 
-app.listen(8001, () => {
-    console.log('Rodando na porta 8001');
+app.post('/produtos', (req, res) => {
+    return res.status(200).send('Cadastrando os produtos!');
 });
+
+app.get('/produtos/:id', (req, res) => {
+    return res.status(200).send('Buscando o produto: ' + req.params.id);
+});
+
+app.put('/produtos/:id', (req, res) => {
+    return res.status(200).send('Atualizando o produto: ' + req.params.id);
+});
+
+app.delete('/produtos/:id', (req, res) => {
+    return res.status(200).send('Removendo o produto: ' + req.params.id);
+});
+
+app.listen(port, () => {
+    console.log(`Ativo na porta: ${port}`);
+})
