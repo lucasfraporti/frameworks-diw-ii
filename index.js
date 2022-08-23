3ad3e1eefb1cc65fa36c0e14e1e206b96b31fc31
lucasfraporti <lucasmfraporti@gmail.com>
2022-08-23 00:35:46 -0300

    add routes

--- index.js
+++ index.js
@@ -2,26 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8001;
 
-app.get('/produtos', (req, res) => {
-    return res.status(200).send('Listando os produtos!');
-});
+const rotaProdutos = require('./routes/routeProduto');
 
-app.post('/produtos', (req, res) => {
-    return res.status(200).send('Cadastrando os produtos!');
-});
+app.use('/rotas', rotaProdutos);
 
-app.get('/produtos/:id', (req, res) => {
-    return res.status(200).send('Buscando o produto: ' + req.params.id);
-});
-
-app.put('/produtos/:id', (req, res) => {
-    return res.status(200).send('Atualizando o produto: ' + req.params.id);
-});
-
-app.delete('/produtos/:id', (req, res) => {
-    return res.status(200).send('Removendo o produto: ' + req.params.id);
-});
 
 app.listen(port, () => {
     console.log(`Ativo na porta: ${port}`);
 })
+
+module.exports = app;
